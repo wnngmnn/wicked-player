a6facdf9319b35d4372e96dc5da35b21a7af8966
wnngmnn <dakarp174@gmail.com>
2026-07-11 23:45:28 -0400

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -77,7 +77,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Blank App" },
+      { title: "Wicked" },
       { name: "description", content: "A blank canvas to build on." },
       { property: "og:title", content: "Blank App" },
       { property: "og:description", content: "A blank canvas to build on." },
